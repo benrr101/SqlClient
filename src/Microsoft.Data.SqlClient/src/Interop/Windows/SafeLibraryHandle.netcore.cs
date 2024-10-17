e674c0f81e9912c58a684101d30f926b6da35fd9
Ben Russell <russellben@microsoft.com>
2024-10-17 18:05:28 -0500

    Move SafeLibraryHandle.cs to common project Move SafeLibraryHandle to Interop namespace

--- src/Microsoft.Data.SqlClient/src/Interop/Windows/SafeLibraryHandle.netcore.cs
+++ src/Microsoft.Data.SqlClient/src/Interop/Windows/SafeLibraryHandle.netcore.cs
@@ -2,15 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace Microsoft.Win32.SafeHandles
+#if !NETFRAMEWORK
+
+using Microsoft.Win32.SafeHandles;
+
+namespace Interop_TEMP.Windows
 {
-    sealed internal class SafeLibraryHandle : SafeHandleZeroOrMinusOneIsInvalid
+    internal sealed class SafeLibraryHandle : SafeHandleZeroOrMinusOneIsInvalid
     {
         internal SafeLibraryHandle() : base(true) { }
 
-        override protected bool ReleaseHandle()
+        protected override bool ReleaseHandle()
         {
             return Interop.Kernel32.FreeLibrary(handle);
         }
     }
 }
+
+#endif
