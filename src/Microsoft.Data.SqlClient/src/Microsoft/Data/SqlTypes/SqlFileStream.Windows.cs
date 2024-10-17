e21c0d801b4bfa10ba4deff3b1d97da45b7c28ed
Ben Russell <russellben@microsoft.com>
2024-10-17 18:32:11 -0500

    Migrating usage os IoControlCodeAccess

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlTypes/SqlFileStream.Windows.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlTypes/SqlFileStream.Windows.cs
@@ -779,7 +779,7 @@ namespace Microsoft.Data.SqlTypes
                 {
                     uint ioControlCode = Interop.Kernel32.CTL_CODE(Interop.Kernel32.FILE_DEVICE_FILE_SYSTEM,
                         IoControlCodeFunctionCode, (byte)IoControlTransferType.METHOD_BUFFERED,
-                        (byte)Interop.Kernel32.IoControlCodeAccess.FILE_ANY_ACCESS);
+                        (byte)IoControlCodeAccess.FILE_ANY_ACCESS);
 
                     if (!Interop.Kernel32.DeviceIoControl(hFile, ioControlCode, IntPtr.Zero, 0, IntPtr.Zero, 0, out uint cbBytesReturned, IntPtr.Zero))
                     {
