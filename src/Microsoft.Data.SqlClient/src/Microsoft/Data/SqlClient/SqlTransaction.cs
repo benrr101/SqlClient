4e71fc7a5a939c4a368097845d4200e2741e6b83
Ben Russell <russellben@microsoft.com>
2024-11-27 18:10:20 -0600

    Merging rollback w/o a transaction name

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlTransaction.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlTransaction.cs
@@ -178,59 +178,29 @@ namespace Microsoft.Data.SqlClient
             base.Dispose(disposing);
         }
         //---netfx
-
-        //netcore---
-        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlTransaction.xml' path='docs/members[@name="SqlTransaction"]/Rollback2/*' />
+        
+        /// <include file='../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlTransaction.xml' path='docs/members[@name="SqlTransaction"]/Rollback2/*' />
         public override void Rollback()
         {
-            using (DiagnosticTransactionScope diagnosticScope = s_diagnosticListener.CreateTransactionRollbackScope(_isolationLevel, _connection, InternalTransaction, null))
-            {
-                if (Is2005PartialZombie)
-                {
-                    // Put something in the trace in case a customer has an issue
-                    SqlClientEventSource.Log.TryAdvancedTraceEvent("SqlTransaction.Rollback | ADV | Object Id {0}, partial zombie no rollback required", ObjectID);
-                    _internalTransaction = null; // 2005 zombification
-                }
-                else
-                {
-                    ZombieCheck();
-
-                    SqlStatistics statistics = null;
-                    using (TryEventScope.Create("SqlTransaction.Rollback | API | Object Id {0}", ObjectID))
-                    {
-                        SqlClientEventSource.Log.TryCorrelationTraceEvent("SqlTransaction.Rollback | API | Correlation | Object Id {0}, ActivityID {1}, Client Connection Id {2}", ObjectID, ActivityCorrelator.Current, Connection?.ClientConnectionId);
-                        try
-                        {
-                            statistics = SqlStatistics.StartTimer(Statistics);
-
-                            _isFromAPI = true;
-
-                            _internalTransaction.Rollback();
-                        }
-                        catch (Exception ex)
-                        {
-                            diagnosticScope.SetException(ex);
-                            throw;
-                        }
-                        finally
-                        {
-                            SqlStatistics.StopTimer(statistics);
-                            _isFromAPI = false;
-                        }
-                    }
-                }
-            }
-        }
-        //---netcore
-        //netfx---
-        /// <include file='../../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlTransaction.xml' path='docs/members[@name="SqlTransaction"]/Rollback2/*' />
-        public override void Rollback()
-        {
+            #if NET
+            const string partialZombieEventMessage = "SqlTransaction.Rollback | ADV | Object Id {0}, partial zombie no rollback required";
+            const string rollbackEventMessage = "SqlTransaction.Rollback | API | Object Id {0}";
+            const string correlationEventMessage = "SqlTransaction.Rollback | API | Correlation | Object Id {0}, ActivityID {1}, Client Connection Id {2}";
+            using DiagnosticTransactionScope diagnosticScope = s_diagnosticListener.CreateTransactionRollbackScope(
+                _isolationLevel,
+                _connection,
+                InternalTransaction,
+                transactionName: null);
+            #else
+            const string partialZombieEventMessage = "<sc.SqlTransaction.Rollback|ADV> {0} partial zombie no rollback required";
+            const string rollbackEventMessage = "<sc.SqlTransaction.Rollback|API> {0}";
+            const string correlationEventMessage = "<sc.SqlTransaction.Rollback|API|Correlation> ObjectID {0}, ActivityID {1}, ClientConnectionId {2}";
+            #endif
+            
             if (Is2005PartialZombie)
             {
                 // Put something in the trace in case a customer has an issue
-                SqlClientEventSource.Log.TryAdvancedTraceEvent("<sc.SqlTransaction.Rollback|ADV> {0} partial zombie no rollback required", ObjectID);
-
+                SqlClientEventSource.Log.TryAdvancedTraceEvent(partialZombieEventMessage, ObjectID);
                 _internalTransaction = null; // 2005 zombification
             }
             else
@@ -238,21 +208,27 @@ namespace Microsoft.Data.SqlClient
                 ZombieCheck();
 
                 SqlStatistics statistics = null;
-                using (TryEventScope.Create("<sc.SqlTransaction.Rollback|API> {0}", ObjectID))
+                using (TryEventScope.Create(rollbackEventMessage, ObjectID))
                 {
-                    SqlClientEventSource.Log.TryCorrelationTraceEvent("<sc.SqlTransaction.Rollback|API|Correlation> ObjectID {0}, ActivityID {1}", ObjectID, ActivityCorrelator.Current);
+                    SqlClientEventSource.Log.TryCorrelationTraceEvent(correlationEventMessage, ObjectID, ActivityCorrelator.Current);
 
+                    #if NETFRAMEWORK
                     TdsParser bestEffortCleanupTarget = null;
                     RuntimeHelpers.PrepareConstrainedRegions();
+                    #endif
+                    
                     try
                     {
+                        #if NETFRAMEWORK
                         bestEffortCleanupTarget = SqlInternalConnection.GetBestEffortCleanupTarget(_connection);
+                        #endif
+
                         statistics = SqlStatistics.StartTimer(Statistics);
-
                         _isFromAPI = true;
-
+                        
                         _internalTransaction.Rollback();
                     }
+                    #if NETFRAMEWORK
                     catch (System.OutOfMemoryException e)
                     {
                         _connection.Abort(e);
@@ -264,66 +240,6 @@ namespace Microsoft.Data.SqlClient
                         throw;
                     }
                     catch (System.Threading.ThreadAbortException e)
-                    {
-                        _connection.Abort(e);
-                        SqlInternalConnection.BestEffortCleanup(bestEffortCleanupTarget);
-                        throw;
-                    }
-                    finally
-                    {
-                        _isFromAPI = false;
-
-                        SqlStatistics.StopTimer(statistics);
-                    }
-                }
-            }
-        }
-        //---netfx
-
-        /// <include file='../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlTransaction.xml' path='docs/members[@name="SqlTransaction"]/RollbackTransactionName/*' />
-        public override void Rollback(string transactionName)
-        {
-            #if NETFRAMEWORK
-            SqlConnection.ExecutePermission.Demand(); // MDAC 81476
-            const string eventScopeMessage = "SqlTransaction.Rollback | API | Object Id {0}, Transaction Name='{1}', ActivityID {2}, Client Connection Id {3}";
-            #else
-            const string eventScopeMessage = <sc.SqlTransaction.Rollback|API> {0} transactionName='{1}', activityId='{2}', clientConnectionId='{3}'";
-            #endif
-            
-            #if NET
-            using (DiagnosticTransactionScope diagnosticScope = s_diagnosticListener.CreateTransactionRollbackScope(_isolationLevel, _connection, InternalTransaction, transactionName))
-            {
-            #endif
-
-                ZombieCheck();
-                SqlStatistics statistics = null;
-
-                using (TryEventScope.Create(SqlClientEventSource.Log.TryScopeEnterEvent(eventScopeMessage, ObjectID, transactionName, ActivityCorrelator.Current, Connection?.ClientConnectionId)))
-                {
-                    #if NETFRAMEWORK
-                    TdsParser bestEffortCleanupTarget = null;
-                    RuntimeHelpers.PrepareConstrainedRegions();
-                    #endif
-
-                    try
-                    {
-                        statistics = SqlStatistics.StartTimer(Statistics);
-                        _isFromAPI = true;
-
-                        _internalTransaction.Rollback(transactionName);
-                    }
-                    #if NETFRAMEWORK
-                    catch (OutOfMemoryException e)
-                    {
-                        _connection.Abort(e);
-                        throw;
-                    }
-                    catch (StackOverflowException e)
-                    {
-                        _connection.Abort(e);
-                        throw;
-                    }
-                    catch (ThreadAbortException e)
                     {
                         _connection.Abort(e);
                         SqlInternalConnection.BestEffortCleanup(bestEffortCleanupTarget);
@@ -342,9 +258,75 @@ namespace Microsoft.Data.SqlClient
                         SqlStatistics.StopTimer(statistics);
                     }
                 }
+            }
+        }
+
+        /// <include file='../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlTransaction.xml' path='docs/members[@name="SqlTransaction"]/RollbackTransactionName/*' />
+        public override void Rollback(string transactionName)
+        {
             #if NET
-            } // Close diagnostic scope using block
+            const string eventScopeMessage = "<sc.SqlTransaction.Rollback|API> {0} transactionName='{1}', activityId='{2}', clientConnectionId='{3}'";
+            using DiagnosticTransactionScope diagnosticScope = s_diagnosticListener.CreateTransactionRollbackScope(
+                _isolationLevel,
+                _connection,
+                InternalTransaction,
+                transactionName);
+            #else
+            const string eventScopeMessage = "SqlTransaction.Rollback | API | Object Id {0}, Transaction Name='{1}', ActivityID {2}, Client Connection Id {3}";
+            SqlConnection.ExecutePermission.Demand(); // MDAC 81476
             #endif
+
+            ZombieCheck();
+            SqlStatistics statistics = null;
+
+            using (TryEventScope.Create(SqlClientEventSource.Log.TryScopeEnterEvent(eventScopeMessage, ObjectID, transactionName, ActivityCorrelator.Current, Connection?.ClientConnectionId)))
+            {
+                #if NETFRAMEWORK
+                TdsParser bestEffortCleanupTarget = null;
+                RuntimeHelpers.PrepareConstrainedRegions();
+                #endif
+
+                try
+                {
+                    #if NETFRAMEWORK
+                    bestEffortCleanupTarget = SqlInternalConnection.GetBestEffortCleanupTarget(_connection);
+                    #endif
+                    
+                    statistics = SqlStatistics.StartTimer(Statistics);
+                    _isFromAPI = true;
+
+                    _internalTransaction.Rollback(transactionName);
+                }
+                #if NETFRAMEWORK
+                catch (OutOfMemoryException e)
+                {
+                    _connection.Abort(e);
+                    throw;
+                }
+                catch (StackOverflowException e)
+                {
+                    _connection.Abort(e);
+                    throw;
+                }
+                catch (ThreadAbortException e)
+                {
+                    _connection.Abort(e);
+                    SqlInternalConnection.BestEffortCleanup(bestEffortCleanupTarget);
+                    throw;
+                }
+                #else
+                catch (Exception ex)
+                {
+                    diagnosticScope.SetException(ex);
+                    throw;
+                }
+                #endif
+                finally
+                {
+                    _isFromAPI = false;
+                    SqlStatistics.StopTimer(statistics);
+                }
+            }
         }
         
         /// <include file='../../../../../../doc/snippets/Microsoft.Data.SqlClient/SqlTransaction.xml' path='docs/members[@name="SqlTransaction"]/Save/*' />
